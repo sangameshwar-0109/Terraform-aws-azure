841204f6348db241c671a795f5d491a0403774f5
sangu <sangu18898@gmail.com>
2024-05-19 14:52:45 +0530

    add

---
+++ inputs.tf
@@ -0,0 +1,43 @@
+variable "webnsg_rules_info" {
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+  default = [{
+    name                       = "allowHttp"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    },
+    {
+      name                       = "AllowHttps"
+      priority                   = "310"
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+
+}
+
+variable "location" {
+  type        = string
+  description = "location"
+  default     = "eastus"
+}
